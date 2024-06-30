6cafd856df3e16af5c78bd09a56d79000bd81172
abdelrahmanlatif04 <abdelrahmanlatif04@gmail.com>
2024-07-01 01:18:28 +0300

    mod

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div
-    :class="`w-full h-[100vh] transition-all  duration-300 relative font-semibold ${mode}`"
+    :class="`w-full min-h-[100vh] items-center  duration-300 relative font-semibold ${mode}`"
   >
     <div
-      :class="` cursor-pointer bg-indigo-300 absolute left-2 top-2 w-16 h-8 rounded-2xl border-orange-400 p-2 flex items-center`"
+      :class="`cursor-pointer bg-indigo-300 absolute left-2 top-2 w-16 h-8 rounded-2xl border-orange-400 p-2 flex items-center`"
       @click="theme = !theme"
     >
       <div
@@ -13,7 +13,7 @@
     </div>
 
     <div
-      class="min-w-64 max-w-72 w-3/5 h-full flex flex-col gap-5 items-center absolute left-1/2 -translate-x-1/2 justify-center"
+      class="min-w-64 max-w-72 mx-auto py-5 w-3/5 h-full flex flex-col justify-around gap-5 items-center"
     >
       <div
         class="card relative w-full aspect-square cursor-pointer transition-all duration-500"
@@ -47,6 +47,7 @@ import XImg from "./assets/twitter.png";
 import linkedInImg from "./assets/linkedin.png";
 import githubImg from "./assets/github.jpg";
 import behanceImg from "./assets/behance.png";
+import whatsappImg from "./assets/whatsapp.svg";
 export default {
   data() {
     return {
@@ -81,6 +82,11 @@ export default {
         {
           img: behanceImg,
           name: "Behance",
+          link: "http://www.behance.net/3bdellatiif",
+        },
+        {
+          img: whatsappImg,
+          name: "Whatsapp",
           link: "http://www.behance.net/3bdellatiif",
         },
       ],
@@ -120,6 +126,6 @@ export default {
 }
 .active {
   transform: translateX(calc(-100% - 8px));
-  @apply bg-white left-full overflow-hidden after:absolute after:left-[17%] after:bottom-[17%] after:w-[85%] after:aspect-square after:rounded-full after:bg-indigo-300;
+  @apply bg-white left-full after:absolute after:right-0 after:top-0 after:w-4/5 after:aspect-square after:rounded-full after:bg-indigo-300;
 }
 </style>
