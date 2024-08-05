99e8d77b10dae9db6062c568c3a4c058d485bbc6
abdelrahmanlatif04 <abdelrahmanlatif04@gmail.com>
2024-08-06 02:42:44 +0300

    mod

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@
     </div>
 
     <div
-      class="min-w-64 max-w-72 mx-auto w-3/5 h-full flex flex-col justify-around items-center"
+      class="min-w-64 max-w-72 mx-auto w-3/5 h-full flex flex-col pt-2 justify-around items-center"
     >
       <div
         class="card relative w-full aspect-square cursor-pointer smooth transform-style-preserve-3d"
@@ -43,18 +43,54 @@ export default {
     return {
       theme: true,
       rotation: null,
-      links: null,
+      links: [
+        {
+          title: "Facebook",
+          img: "./src/assets/facebook.svg",
+          dist: "https://www.facebook.com/profile.php?id=100003984241851&mibextid=ZbWKwL",
+        },
+        {
+          title: "Instagram",
+          img: "./src/assets/instagram.jpg",
+          dist: "https://www.instagram.com/3bdellatiif04",
+        },
+        {
+          title: "X (Twitter)",
+          img: "./src/assets/twitter.png",
+          dist: "https://twitter.com/3bdellatiif",
+        },
+        {
+          title: "LinkedIn",
+          img: "./src/assets/linkedin.png",
+          dist: "https://www.linkedin.com/in/abdelrahmanlatif/",
+        },
+        {
+          title: "Github",
+          img: "./src/assets/github.jpg",
+          dist: "https://github.com/abdelrahmanlatif04",
+        },
+        {
+          title: "Whatsapp",
+          img: "./src/assets/whatsapp.svg",
+          dist: "https://wa.me/+201070039593",
+        },
+        {
+          title: "Behance",
+          img: "./src/assets/behance.png",
+          dist: "behance.net/3bdellatiif",
+        },
+      ],
     };
   },
-  created() {
-    fetch("../links.json")
-      .then((response) => {
-        return response.json();
-      })
-      .then((res) => {
-        this.links = res;
-      });
-  },
+  // created() {
+  //   fetch("../links.json")
+  //     .then((response) => {
+  //       return response.json();
+  //     })
+  //     .then((res) => {
+  //       this.links = res;
+  //     });
+  // },
   methods: {
     rotateCard() {
       this.rotation =
