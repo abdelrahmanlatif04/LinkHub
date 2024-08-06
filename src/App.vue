3b0fe0d7c1a1c2fd4904dff73fea71388102a7f6
abdelrahmanlatif04 <abdelrahmanlatif04@gmail.com>
2024-08-06 15:13:48 +0300

    mod

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,11 @@
       @click="theme = !theme"
     >
       <div
-        class="w-6 rounded-full toggle aspect-square absolute left-2 bg-[#fa0] smooth"
-        :class="theme ? 'active' : ''"
+        :class="`w-6 rounded-full toggle aspect-square absolute left-2 bg-[#fa0] smooth ${
+          theme ? 'active' : ''
+        }`"
       ></div>
     </div>
-
     <div
       class="min-w-64 max-w-72 mx-auto w-3/5 h-full flex flex-col pt-2 justify-around items-center"
     >
@@ -46,7 +46,7 @@ export default {
       links: [
         {
           title: "Facebook",
-          img: "src/components/linksImgs/facebook.svg",
+          img: "./src/components/linksImgs/facebook.svg",
           dist: "https://www.facebook.com/profile.php?id=100003984241851&mibextid=ZbWKwL",
         },
         {
