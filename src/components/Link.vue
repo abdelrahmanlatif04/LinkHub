1a1414328acc08fd71871e5c928d2f423dfe7920
abdelrahmanlatif04 <abdelrahmanlatif04@gmail.com>
2024-08-07 03:07:13 +0300

    mod

--- src/components/Link.vue
+++ src/components/Link.vue
@@ -2,7 +2,7 @@
   <a
     :href="link.dist"
     target="_blank"
-    class="w-full bg-white cursor-pointer rounded-lg pr-2 pl-5 hover:pl-16 smooth py-3 flex h-10 hover:h-14 items-center gap-2 relative overflow-hidden before:h-full before:absolute before:aspect-square before:-left-10 hover:before:left-0 before:smooth before:bg-[#fa0] hover:before:rounded-lg hover:shadow-inner"
+    class="w-full bg-white cursor-pointer rounded-lg pr-2 pl-5 hover:pl-16 smooth py-3 flex h-10 hover:h-14 items-center gap-2 relative overflow-hidden before:h-full before:absolute before:aspect-square before:-left-8 hover:before:left-0 before:smooth before:bg-[#fa0] hover:before:rounded-lg hover:shadow-inner"
   >
     <img :src="link.img" class="h-[90%]" />
     {{ link.title }}
