09726fb5b77c74e2807ceb23ae57b576b6f82cdb
abdelrahmanlatif04 <abdelrahmanlatif04@gmail.com>
2024-08-07 04:57:17 +0300

    mod

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div
     :class="`w-full min-h-[100vh] items-center smooth relative font-semibold ${mode}`"
   >
-    <div
+    <!-- <div
       :class="`cursor-pointer bg-indigo-300 md:relative absolute  left-2 top-2 w-16 h-8 rounded-2xl border-orange-400 p-2 flex items-center`"
       @click="changeMood()"
     >
@@ -11,7 +11,7 @@
           mode == 'bg-dark' ? 'active' : ''
         }`"
       ></div>
-    </div>
+    </div> -->
     <div
       class="min-w-64 max-w-72 mx-auto w-3/5 h-full flex flex-col pt-2 justify-around items-center"
     >
