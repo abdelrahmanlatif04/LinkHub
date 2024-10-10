72d28f5d466a5775bb8929c552f9fe99f2ffa6ed
abdelrahmanlatif04 <abdelrahmanlatif04@gmail.com>
2024-10-10 03:27:56 +0300

    mod

--- src/components/Link.vue
+++ src/components/Link.vue
@@ -2,7 +2,7 @@
   <a
     :href="link.dist"
     target="_blank"
-    class="w-full bg-white cursor-pointer rounded-lg pr-2 pl-5 hover:pl-16 smooth py-3 flex h-12 hover:h-14 items-center text-lg hover:text-xl gap-3 relative overflow-hidden before:h-full before:absolute before:aspect-square before:-left-10 hover:before:left-0 before:smooth before:bg-[#fa0] hover:before:rounded-lg shadow-lg my-1 hover:my-0 before:bg-no-repeat before:bg-center before:bg-[url('/public/link.png')]"
+    class="w-full bg-white cursor-pointer rounded-lg pr-2 pl-5 hover:pl-16 smooth py-3 flex h-12 hover:h-14 items-center text-lg hover:text-xl gap-3 relative overflow-hidden before:h-full before:absolute before:aspect-square before:-left-10 hover:before:left-0 before:smooth before:bg-[#fa0] hover:before:rounded-lg shadow-lg my-1 hover:my-0 before:bg-no-repeat before:bg-center before:bg-[url('/link.png')]"
   >
     <img :src="link.img" class="h-[90%]" />
     <p>
